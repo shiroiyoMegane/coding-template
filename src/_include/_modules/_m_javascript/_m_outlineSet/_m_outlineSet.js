6d9e2c733d843037e50157003bda6784fa725f34
NP-nishikawa <nishikawa@nxpg.net>
2020-06-17 14:35:40 +0900

    no message

---
+++ src/_include/_modules/_m_javascript/_m_outlineSet/_m_outlineSet.js
@@ -0,0 +1,42 @@
+// import globalSet from '../_m_globalSet/_m_globalSet.js';
+// globalSet();
+// let _g = window.GLOBAL;
+
+window.C_OUTLINESET = {
+	TARGET: null,
+	DEFAULT: {
+		target: '.l-columnBlock',
+	},
+	OPTION: null,
+	init: function(op){
+		let _c = this;
+			_c.OPTION = Object.assign(_c.DEFAULT, op);
+			_c.TARGET = document.querySelectorAll(_c.OPTION.target);
+
+			$(window).on("keydown", function(e) {
+				if(event.altKey){
+				  if(e.keyCode === 79){
+					$(_c.TARGET).css({
+						'outline': '2px solid red'
+					})
+					 return false;
+				  }
+				}
+			});
+			$(window).on("keyup", function(e) {
+				if(event.altKey){
+				  if(e.keyCode === 79){
+					$(_c.TARGET).css({
+						'outline': 'none'
+					})
+					 return false;
+				  }
+				}
+			});
+	}
+}
+
+export default function(op) {
+	window.C_OUTLINESET.init(op);
+}
+
